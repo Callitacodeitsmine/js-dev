8ad88b00a9eabf7b31bc06e251755f07e8e9364a
justaratherveryusefulrepo <139318991+justaratherveryusefulrepo@users.noreply.github.com>
2023-07-12 11:49:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let clickCount = 0;
+
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function() {
+  clickCount++;
+  console.log('Click count:', clickCount);
+});
